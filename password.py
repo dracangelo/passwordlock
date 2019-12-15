8c253b892a130a9befe0ed7dda48ca97352ebc72
dracangelo <kelvinmbuguaw@gmail.com>
2019-12-15 16:40:00 +0300

    added a define init  to password.py

--- password.py
+++ password.py
@@ -6,3 +6,7 @@ class Password:
     pass
     Password_array = []
 
+    def __init__(self, user_name,password,email):
+        self.user_name = user_name
+        self.password = password
+        self.email = email
