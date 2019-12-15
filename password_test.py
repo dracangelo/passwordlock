dc6770e6fd963e6bae7a0ff7aaf6bdcafdbdd5fe
dracangelo <kelvinmbuguaw@gmail.com>
2019-12-15 17:09:01 +0300

    added a def setup and teardown

--- password_test.py
+++ password_test.py
@@ -2,3 +2,12 @@ import unittest
 from password import Password
 
 class testUser(unittest.TestCase):
+    def setUp(self):
+    """
+    set up to run before each test case.
+
+    """
+    self.new_password = Password("user_name","password","email@ms.com") #this creates a new password
+
+    def tearDown(self)
+    """cleans up after each case has run
