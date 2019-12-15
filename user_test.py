1a998ae5700d40a4a7fbf3083b0223d60fac2690
dracangelo <kelvinmbuguaw@gmail.com>
2019-12-15 22:01:16 +0300

    added  a def to test initialization

--- user_test.py
+++ user_test.py
@@ -11,3 +11,11 @@ class TestUser(unittest.TestCase):
             create a user account before each test
             """
             self.new user = User('Drac','Angelo','memedank2')
+
+    def test__init__(self):
+            """
+            test if the initialization of the user has been done correctly
+            """
+            self.assertEqual(self.new_user.first_name,'Drac')
+            self.assertEqual(self.new_user.last_name,'Angelo')
+            self.assertEqual(self.new_password.password,'memedank2')
