933c72304806cdbd3a80045cad943b6dec851b00
dracangelo <kelvinmbuguaw@gmail.com>
2019-12-16 08:31:05 +0300

    added a loop

--- user_test.py
+++ user_test.py
@@ -2,43 +2,55 @@ import unittest
 import pyperclip
 from user import User
 
+
 class TestUser(unittest.TestCase):
     """
     testcase helps in createng text cases
     """
+
     def setUp(self):
-            """
-            create a user account before each test
-            """
-            self.new user = User('Drac','Angelo','memedank2')
+        """
+        create a user account before each test
+        """
+        self.new_user = User('Drac', 'Angelo', 'memedank2')
 
     def test__init__(self):
-            """
-            test if the initialization of the user has been done correctly
-            """
-            self.assertEqual(self.new_user.first_name,'Drac')
-            self.assertEqual(self.new_user.last_name,'Angelo')
-            self.assertEqual(self.new_password.password,'memedank2')
+        """
+        test if the initialization of the user has been done correctly
+        """
+        self.assertEqual(self.new_user.first_name, 'Drac')
+        self.assertEqual(self.new_user.last_name, 'Angelo')
+        self.assertEqual(self.new_password.password, 'memedank2')
 
     def test_save_user(self):
-            '''
-            test if user's info has been saved inthe user's list
-            '''
-            self.new_user.save_user()
-            self.assertEqual(len(User.user_list),1)
+        '''
+        test if user's info has been saved inthe user's list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
+
 
 class TestPassword(unit.TestCase):
-            """
-            class defines test cases while testCase  creates a test case
-            """
-            def test_check_user(self):
-                """
-                tests whether the login function works as expected
-                """
-                self.new_user = User ('Drac','Angelo','memedank2')
-                self.new_user.save_user()
-                user2 = User('Ken','Ng\'ang\'a','murigi')
-		        user2.save_user()
+    """
+    class defines test cases while testCase  creates a test case
+    """
 
-if __name__ ==  '__main__':
+    def test_check_user(self):
+        """
+        tests whether the login function works as expected
+        """
+        self.new_user = User('Drac', 'Angelo', 'memedank2')
+        self.new_user.save_user()
+        user2 = User('Ken', 'Ng\'ang\'a', 'murigi')
+        user2.save_user()
+
+        for User in User.user_list:
+            if user.first_name == user2.first_name and user.password == user2.password:
+                current_user == first_name
+            
+            return current_user
+
+            self.assertEqual(current_user, password.check_user(user2.password, user2.first_name))
+
+if __name__ == '__main__':
     unittest.main()
