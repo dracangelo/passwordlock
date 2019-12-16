3fd23a294294e9496131b549ee785dcab9d59edf
dracangelo <kelvinmbuguaw@gmail.com>
2019-12-16 16:44:06 +0300

    indented code

--- password.py
+++ password.py
@@ -12,9 +12,9 @@ def create_user(first_name, last_name, password):
 
 
 def save_user(user):
-    '''
+	"""
     this saves a new user
-    '''
+    """
     User.save_user
 
 
@@ -27,39 +27,39 @@ def check_user(first_name, password):
 
 
 def generate_password():
-    '''
+	"""
     Function to generate a password automatically
-    '''
+    """
     gen_pass = Credential.generate_password()
     return gen_pass
 
 
 def create_credential(user_name, media_name, account_name, password):
-    '''
+	"""
     Function to create a new credential
-    '''
+    """
     new_credential = Credential(user_name, media_name, account_name, password)
     return new_credential
 
 
 def save_credential(credential):
-    '''
+	"""
     Function to save a newly created credential
-    '''
+    """
     Credential.save_credentials(credential)
 
 
 def display_credentials(user_name):
-    '''
+	"""
     Function to display credentials saved by a user
-    '''
+    """
     return Credential.display_credentials(user_name)
 
 
 def copy_credential(media_name):
-    '''
+	"""
     Function to copy a credentials details to the clipboard
-    '''
+    """
     return Credential.copy_credential(media_name)
 
 
@@ -90,13 +90,9 @@ def main():
 			print('To login, enter your account details:')
 			user_name = input('Enter your first name - ').strip()
 			password = str(input('Enter your password - '))
-			user_exists = verify_user(user_name,password)
+			user_exists = check_user(user_name,password)
 			if user_exists == user_name:
 				print(" ")
 				print(f'Welcome {user_name}. Please choose an option to continue.')
 				print(' ')
 				while True:
-
-
-if __name__ == '__main__':
-    main()
