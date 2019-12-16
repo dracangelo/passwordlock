83996c46f135d21862fb09fe3fa472ea21c04851
dracangelo <kelvinmbuguaw@gmail.com>
2019-12-16 14:48:48 +0300

    added a class method

--- user.py
+++ user.py
@@ -10,9 +10,12 @@ class User:
     generates a new user
     """
     pass
-user_list = []
+users_list = []
 
 def __init__(self,first_name,last_name,password):
+    """
+    method defines the property for each object to hold
+    """
     self.first_name = first_name
     self.last_name = last_name
     self.password = password
@@ -21,6 +24,23 @@ def save_user_details(self):
     """
     saves into user array
     """
-    User.user_list.append(self)
+    User.users_list.append(self)
 
+
+class credential:
+    """
+    used to generate the credential
+    """
+    credentials_list = []
+    user_credentials_list = []
+    @classmethod
+    def check_user(cls,first_name,password):
+        '''
+        checks if the name and password entered match the entries in the users list
+        '''
+        current_user = ''
+        for user in users_list:
+            if(user.first_name == first_name and user_password == password):
+                current_user = user.first_name
+        return current_user
 
