17a91f2c07854a7a425191fc4fe4bef56f78b764
dracangelo <kelvinmbuguaw@gmail.com>
2019-12-16 16:11:55 +0300

    added how to get credentials

--- password.py
+++ password.py
@@ -22,7 +22,36 @@ def check_user(first_name,password)
     verify_user = Credential.veryfy_user(first_name,password)
     return verify_user
 
+def generate_password():
+	'''
+	Function to generate a password automatically
+	'''
+	gen_pass = Credential.generate_password()
+	return gen_pass
 
+def create_credential(user_name,media_name,account_name,password):
+	'''
+	Function to create a new credential
+	'''
+	new_credential=Credential(user_name,media_name,account_name,password)
+	return new_credential
 
+def save_credential(credential):
+	'''
+	Function to save a newly created credential
+	'''
+	Credential.save_credentials(credential)
+
+def display_credentials(user_name):
+	'''
+	Function to display credentials saved by a user
+	'''
+	return Credential.display_credentials(user_name)
+	
+def copy_credential(media_name):
+	'''
+	Function to copy a credentials details to the clipboard
+	'''
+	return Credential.copy_credential(media_name)
 if __name__ == '__main__':
 	main()
