f5cea9c58b9a015411cdde5865371a71bab5530d
dracangelo <kelvinmbuguaw@gmail.com>
2019-12-16 15:58:47 +0300

    added a check user

--- password.py
+++ password.py
@@ -2,16 +2,27 @@
 import pyperclip
 from user import User
 
-def create user(first_name, last_name, password)
+def create_user(first_name, last_name, password):
     """
     this creates a new account user
     """
-    new_user = user(first_name,last_name,password)
-    return new_user
+    create_user = User(first_name,last_name,password)
+    return create_user
 
-def save_user(user)
+def save_user(user):
     '''
     this saves a new user
     '''
     User.save_user
+    
+def check_user(first_name,password)
+    """
+    checks existence of a user before creating credentials
+    """
+    verify_user = Credential.veryfy_user(first_name,password)
+    return verify_user
 
+
+
+if __name__ == '__main__':
+	main()
