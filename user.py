d789e3fc277e986393a9ed7273bd906b485225d4
dracangelo <kelvinmbuguaw@gmail.com>
2019-12-16 15:07:20 +0300

    added a def to initialize and save users

--- user.py
+++ user.py
@@ -2,45 +2,63 @@ import pyperclip
 import random
 import string
 
-#global variables
-
-global user_list
+# Global Variables
+global users_list 
 class User:
-    """
-    generates a new user
-    """
-    pass
-users_list = []
+	'''
+	Class to create user accounts and save their information
+	'''
+	# Class Variables
+	# global users_list
+	users_list = []
+	def __init__(self,first_name,last_name,password):
+		'''
+		Method to define the properties for each user object will hold.
+		'''
 
-def __init__(self,first_name,last_name,password):
-    """
-    method defines the property for each object to hold
-    """
-    self.first_name = first_name
-    self.last_name = last_name
-    self.password = password
+		# instance variables
+		self.first_name = first_name
+		self.last_name = last_name
+		self.password = password
 
-def save_user_details(self):
-    """
-    saves into user array
-    """
-    User.users_list.append(self)
+	def save_user(self):
+		'''
+		Function to save a newly created user instance
+		'''
+		User.users_list.append(self)
+		
+class Credential:
+	'''
+	Class to create  account credentials, generate passwords and save their information
+	'''
+	# Class Variables
+	credentials_list =[]
+	user_credentials_list = []
+	@classmethod
+	def check_user(cls,first_name,password):
+		'''
+		Method that checks if the name and password entered match entries in the users_list
+		'''
+		current_user = ''
+		for user in User.users_list:
+			if (user.first_name == first_name and user.password == password):
+				current_user = user.first_name
+		return current_user
+        
+    def __init__(self,user_name,media_name,account_name,password):
+        """
+        Method to define the properties for each user object will hold.
+		"""
 
-
-class credential:
-    """
-    used to generate the credential
-    """
-    credentials_list = []
-    user_credentials_list = []
-    @classmethod
-    def check_user(cls,first_name,password):
+		self.user_name = user_name
+		self.site_name = site_name
+		self.account_name = account_name
+        self.password = password
+        
+    def save_credentials()
         '''
-        checks if the name and password entered match the entries in the users list
+        save new users
         '''
-        current_user = ''
-        for user in users_list:
-            if(user.first_name == first_name and user_password == password):
-                current_user = user.first_name
-        return current_user
-
+        Credential.credentials_list.append(self)
+        
+    
